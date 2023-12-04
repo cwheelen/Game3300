ed26abb2effe002169880e87d16acaaebf7dabe0
Annie Zhang <zhang.annie1@northeastern.edu>
2023-12-04 15:01:01 -0500

    made recipe

--- Script.js
+++ Script.js
@@ -24,8 +24,24 @@ function hearts(h) {
 
 		number++;
 
-		document.getElementById("hNum2").innerHTML = number;
-	} else {
+		document.getElementById("hNum3").innerHTML = number;
+	} else if (h == "pudding") {
+		hNum3 = document.getElementById("hNum3");
+		number = hNum3.innerHTML;
+		console.log(number);
+
+		number++;
+
+		document.getElementById("hNum4").innerHTML = number;
+	} else if (h == "sweet_rice") {
+		hNum4 = document.getElementById("hNum4");
+		number = hNum4.innerHTML;
+		console.log(number);
+
+		number++;
+
+		document.getElementById("hNum4").innerHTML = number;
+	}else {
 		console.log("this is not supposed to happen.")
 	}
 }
